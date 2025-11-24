334333f4c1e45f6aee372f884f0f7d5fe748afe5
ja5perlu <ja5perlu@users.noreply.github.com>
2025-11-24 06:28:02 +0000

    deploy: d9d122f45decc1cc21689e09f2568b1c71994547

---
+++ _nuxt/B0N_vPWZ.js
@@ -0,0 +1,1 @@
+const t={welcome:{t:0,b:{t:2,i:[{t:3}],s:"Welcome"}},test:{t:0,b:{t:2,i:[{t:3}],s:"Test"}}};export{t as default};
---
+++ _nuxt/CQY9_NBN.js
@@ -0,0 +1,1 @@
+import{d as e,t,v as n}from"./DVtReH2W.js";const s=e({__name:"index",setup(o){return(a,r)=>(n(),t("div"))}});export{s as default};
